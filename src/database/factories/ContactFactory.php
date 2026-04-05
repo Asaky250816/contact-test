4e08ca092f52cd05ba63cabd7407e007e7218070
asaky <asaukiuki@gmail.com>
2026-04-05 22:42:51 +0900

    Add models, factories, and seeders for categories and contacts

---
+++ src/database/factories/ContactFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ContactFactory extends Factory
+{
+    protected $model = \App\Models\Contact::class;
+
+    public function definition()
+    {
+        return [
+            'category_id' => $this->faker->numberBetween(1, 5),
+            'first_name' => $this->faker->lastName,
+            'last_name' => $this->faker->firstName,
+            'gender' => $this->faker->numberBetween(1, 3),
+            'email' => $this->faker->safeEmail,
+            'tel' => $this->faker->phoneNumber,
+            'address' => $this->faker->address,
+            'building' => $this->faker->secondaryAddress,
+            'detail' => $this->faker->realText(100),
+        ];
+    }
+}
